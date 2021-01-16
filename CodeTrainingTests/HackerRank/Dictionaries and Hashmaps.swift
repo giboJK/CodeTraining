8af8fb54ff82367d11de47e01e171b31832bc8b1
giboJK <teoeirm@gmail.com>
2021-01-16 10:21:30 +0900

    SherlockAndAnagrams

--- CodeTrainingTests/HackerRank/Dictionaries and Hashmaps.swift
+++ CodeTrainingTests/HackerRank/Dictionaries and Hashmaps.swift
@@ -43,10 +43,39 @@ class DictionariesAndHashmaps: XCTestCase {
     // Sherlock and Anagrams
     
     func test_sherlockAndAnagrams() {
-        
+        var s: String
+        s = "abba"
+        XCTAssertEqual(4, sherlockAndAnagrams(s: s))
+
+        s = "abcd"
+        XCTAssertEqual(0, sherlockAndAnagrams(s: s))
+
+        s = "cdcd"
+        XCTAssertEqual(5, sherlockAndAnagrams(s: s))
     }
     
-    func sherlockAndAnagrams() {
+    func sherlockAndAnagrams(s: String) -> Int {
+        var ans = 0
+        var subStrings = [[Character]: Int]()
         
+        for i in 0 ..< s.count {
+            for j in i ..< s.count {
+                let startIndex = s.index(s.startIndex, offsetBy: i)
+                let endIndex = s.index(startIndex, offsetBy: j - i + 1)
+                let chars = s[startIndex..<endIndex].sorted()
+                
+                if let count = subStrings[chars] {
+                    subStrings.updateValue(count + 1, forKey: chars)
+                } else {
+                    subStrings.updateValue(1, forKey: chars)
+                }
+            }
+        }
+        
+        subStrings.values.forEach { v in
+            ans += v * (v - 1) / 2
+        }
+
+        return ans
     }
 }
