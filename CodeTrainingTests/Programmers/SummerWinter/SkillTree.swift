a1a2487b50a89f74dd8f291d5454693e55ca38a7
giboJK <teoeirm@gmail.com>
2021-02-18 23:06:06 +0900

    skill tree

--- CodeTrainingTests/Programmers/SummerWinter/SkillTree.swift
+++ CodeTrainingTests/Programmers/SummerWinter/SkillTree.swift
@@ -15,6 +15,26 @@ class SkillTree: XCTestCase {
         skill = "CBD"
         skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
         XCTAssertEqual(2, solution(skill, skill_trees))
+        
+        skill = "CBD"
+        skill_trees = ["AEF", "ZJW"]
+        XCTAssertEqual(2, solution(skill, skill_trees))
+        
+        skill = "REA"
+        skill_trees = ["REA", "POA"]
+        XCTAssertEqual(1, solution(skill, skill_trees))
+        
+        skill = "CBDK"
+        skill_trees = ["CB", "CXYB", "BD", "AECD", "ABC", "AEX", "CDB", "CBKD", "IJCB", "LMDK"]
+        XCTAssertEqual(4, solution(skill, skill_trees))
+        
+        skill = "BDC"
+        skill_trees = ["AAAABAAC"]
+        XCTAssertEqual(0, solution(skill, skill_trees))
+        
+        skill = "CBD"
+        skill_trees = ["C", "D", "CB", "BDA"]
+        XCTAssertEqual(2, solution(skill, skill_trees))
     }
     
     func solution(_ skill: String, _ skill_trees: [String]) -> Int {
@@ -29,12 +49,23 @@ class SkillTree: XCTestCase {
                     temp.append(s)
                 }
             }
+            if temp.isEmpty {
+                continue
+            }
             if !skill.contains(temp) || skill.first != temp.first {
                 ans -= 1
             }
-            
         }
         
         return ans
     }
+    
+//    func solution(_ skill: String, _ skill_trees: [String]) -> Int {
+//        return skill_trees.filter {
+//            let filteredSkill = $0.filter {
+//                return skill.firstIndex(of: $0) != nil
+//            }
+//            return skill.hasPrefix(filteredSkill)
+//        }.count
+//    }
 }
